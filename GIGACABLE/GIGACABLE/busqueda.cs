68c5272feec502a7d20ce7174d06dd581654a695
irbiing <irving.bravo@alumnos.udg.mx>
2019-04-09 22:49:24 -0500

    mejora a la ventana de tecnico

---
+++ GIGACABLE/GIGACABLE/busqueda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GIGACABLE
+{
+    public partial class busqueda : Form
+    {
+        public busqueda()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GIGACABLE/GIGACABLE/usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GIGACABLE
+{
+    public partial class usuario : Form
+    {
+        public usuario()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            busqueda llamada2 = new busqueda();
+            llamada2.ShowDialog();
+            if (llamada2.DialogResult == DialogResult.Yes)
+            {
+
+            }
+        }
+    }
+}
